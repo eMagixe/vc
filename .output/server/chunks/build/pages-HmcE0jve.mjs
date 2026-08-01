c6e2aaae7791acb55cf8c448b26becceae3cce3c
magixe <emax.mails@gmail.com>
2026-08-01 19:15:05 +0500

    Init build

---
+++ .output/server/chunks/build/pages-HmcE0jve.mjs
@@ -0,0 +1,48 @@
+import { v as vue_exports } from '../virtual/entry.mjs';
+import { _ as _plugin_vue_export_helper_default } from './_plugin-vue_export-helper-BOaGB7Aw.mjs';
+import 'unhead/plugins';
+import 'unhead/utils';
+import 'vue';
+import '../routes/renderer.mjs';
+import '../nitro/nitro.mjs';
+import 'node:http';
+import 'node:https';
+import 'node:crypto';
+import 'stream';
+import 'events';
+import 'http';
+import 'crypto';
+import 'buffer';
+import 'zlib';
+import 'https';
+import 'net';
+import 'tls';
+import 'url';
+import 'node:events';
+import 'node:buffer';
+import 'node:fs';
+import 'node:path';
+import 'node:url';
+import '@iconify/utils';
+import 'consola';
+import 'unhead/server';
+import 'unhead/legacy';
+import 'nostics';
+import 'vue-bundle-renderer/runtime';
+import 'vue/server-renderer';
+import 'devalue';
+import 'tailwindcss/colors';
+
+//#region app/pages/index.vue
+var _sfc_main = {};
+function _sfc_ssrRender(_ctx, _push, _parent, _attrs) {}
+var _sfc_setup = _sfc_main.setup;
+_sfc_main.setup = (props, ctx) => {
+	const ssrContext = (0, vue_exports.useSSRContext)();
+	(ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("pages/index.vue");
+	return _sfc_setup ? _sfc_setup(props, ctx) : void 0;
+};
+var pages_default = /*#__PURE__*/ _plugin_vue_export_helper_default(_sfc_main, [["ssrRender", _sfc_ssrRender]]);
+
+export { pages_default as default };
+//# sourceMappingURL=pages-HmcE0jve.mjs.map
